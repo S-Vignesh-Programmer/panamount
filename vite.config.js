2cb73bd0abe16ed01698c854926d07137458ca33
VIGNESH-S-J2EE <vigneshjava69@gmail.com>
2025-06-22 18:03:13 +0530

    project

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: '/panamount/',
   plugins: [react()],
+  base: "/panamount"
 });
 
